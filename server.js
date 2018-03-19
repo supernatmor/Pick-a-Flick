e08f9dcc38b893734bc1681fe6ad0dc0af0a00ac
Nate Morgan <natmor88@gmail.com>
2018-03-18 21:49:15 -0400

    removed unnecessary server and package-lock files

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-var app = express();
-
-app.use('/', express.static(__dirname));
-
-app.listen(5000, function() {
-    console.log('Server listening on http://localhost:5000/');
-});
